21eea6cbacb8a9c064238e4bd64cd8c3048d86c9
Asche Jurotich <asche.jurotich@gmail.com>
2024-05-02 21:36:17 -0500

    set up classes and outline

---
+++ Dungeon/Dungeon/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using DungeonLibrary;
+
+namespace Dungeon {
+	class Program {
+		static void Main(string[] args) {
+
+			//=== TITLE/HEADER ===\\
+			Signature.Header("DUNGEON", "Your adventure awaits...");
+
+			//=== VARIABLES ===\\
+			int score = 0;
+
+			//TODO player
+
+
+			//TODO weapons
+
+
+			//TODO game loop
+			// - exit condition
+			do {
+				//TODO generate random room
+
+				//TODO select monster
+
+				do {
+					//TODO gameplay menu options
+					switch("") {
+						case "combat": {
+							//TODO combat
+							break;
+						}
+
+						case "runaway": {
+							//TODO run away
+							break;
+						}
+
+						case "playerinfo": {
+							//TODO player info
+							break;
+						}
+
+						case "monsterinfo": {
+							//TODO monsterinfo
+							break;
+						}
+
+						case "checklife": {
+							//TODO check life
+							break;
+						}
+
+						case "exit": {
+							//TODO exit
+							break;
+						}
+
+						default: {
+							//TODO default/invalid
+							break;
+						}
+
+
+					}
+
+					// exit condition
+
+				} while(false);
+
+				//TODO output final score
+
+			} while(false);
+
+			Signature.Footer();
+
+		}
+	}
+}
---
+++ Dungeon/DungeonLibrary/Signature.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DungeonLibrary {
+	public class Signature {
+
+		public static void Header(string title) {
+			if(title == "") return;
+
+			Console.Title = title;
+			Console.WriteLine("\n" + title);
+			for(int i = 0;i<title.Length;i++) Console.Write("-");
+			Console.WriteLine("\n");
+		}
+
+		public static void Header(string title, string description) {
+			if(title == "") return;
+
+			Console.Title = title;
+			Console.WriteLine("\n" + title);
+			Console.WriteLine(description);
+			for(int i = 0;i<description.Length;i++) Console.Write("-");
+			Console.WriteLine("\n");
+		}
+
+		public static void Footer() {
+			Console.Clear();
+			Console.WriteLine("\n\n\n\n");
+			Console.WriteLine("Thanks for playing!");
+			Console.WriteLine("Created by Alias Jurotich");
+			Console.WriteLine("\n\n\n\n");
+		}
+
+	}
+}
