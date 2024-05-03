d8839a4340e00b99dd79d37e00caa93bad0e27b6
Asche Jurotich <asche.jurotich@gmail.com>
2024-05-03 00:28:25 -0500

    Entity, Race, Armour, Weapon classes filled out with basic information and placeholder values

---
+++ Dungeon/DungeonLibrary/Entity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary {
+
+    public class Entity {
+
+        //=== FIELDS ===\\
+		private string _name;
+		private float _health, _skill;
+		private bool _isAlive;
+		private Race _race;
+		private Armour _armour;
+		private Weapon _weapon;
+
+        //=== PROPERTIES ===\\
+        public string Name	 => _name;
+        public float Health	 => _health;
+        public float Skill	 => _skill;
+        public bool IsAlive	 => _isAlive;
+		public Race Race	 => _race;
+		public Armour Armour => _armour;
+		public Weapon Weapon => _weapon;
+
+        //=== CTOR ===\\
+        public Entity(string name, Race race, Armour armour, Weapon weapon) {
+            _name = name;
+
+            _race = race;
+			_health = race.MaxHealth;
+			_skill = race.MaxSkill;
+			_isAlive = true;
+
+			_armour = armour;
+			_weapon = weapon;
+        }
+
+        //=== METHODS ===\\
+        public float Damage(float damageAmount) {
+			_health -= damageAmount;
+
+            if(Health < 0) _isAlive = false;
+
+            return Health;
+        }
+		public float Heal(float healAmount) {
+			_health += healAmount;
+
+			if(_health < Race.MaxHealth) _health = Race.MaxHealth;
+
+			return Health;
+		}
+		//todo attack
+		//todo block
+		public void ChangeArmor(Armour armor) {
+			_armour = armor;
+		}
+		public void ChangeWeapon(Weapon weapon) {
+			_weapon = weapon; 
+		}
+    }
+}
--- Dungeon/DungeonLibrary/Weapon.cs
+++ Dungeon/DungeonLibrary/Weapon.cs
@@ -5,6 +5,90 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DungeonLibrary {
-	class Weapon {
+
+	public enum WeaponType {
+		Daggers, //low damage, easy
+		Sword, //midlow damage, pretty easy
+		Axe, // midhigh damage
+		Hammer // hard
+	}
+	public enum WeaponMod {
+		Epic,
+		New,
+		Used,
+		Old,
+		Broken
+	}
+
+	public class Weapon {
+
+		//=== FIELDS ===\\
+		private WeaponType _type;
+		private WeaponMod _mod;
+		private float _damage, _difficulty;
+
+		//=== PROPS ===\\
+		public WeaponMod WeaponMod		=> _mod;
+		public WeaponType WeaponType	=> _type;
+		public float Damage				=> _damage;
+		public float Difficulty			=> _difficulty;
+
+		//=== CTOR ===\\
+		public Weapon(WeaponType type) {
+			_type = type;
+			_mod = RandomMod();
+
+			switch(_type) {
+				case WeaponType.Daggers:
+					_damage = 10;
+					_difficulty = 5;
+					break;
+				case WeaponType.Sword:
+					_damage = 15;
+					_difficulty = 10;
+					break;
+				case WeaponType.Axe:
+					_damage = 20;
+					_difficulty = 15;
+					break;
+				case WeaponType.Hammer:
+					_damage = 25;
+					_difficulty = 20;
+					break;
+			}
+			switch(_mod) {
+				case WeaponMod.Epic:
+					_damage *= 1.15f;
+					break;
+				case WeaponMod.New:
+					_damage *= 1.05f;
+					break;
+				case WeaponMod.Used:
+					break;
+				case WeaponMod.Old:
+					_damage *= .95f;
+					break;
+				case WeaponMod.Broken:
+					_damage *= .85f;
+					break;
+			}
+		}
+
+		//=== METHODS ===\\
+		public WeaponType RandomType() {
+			Random random = new Random();
+			Array values = Enum.GetValues(typeof(WeaponType));
+			WeaponType rType = (WeaponType)values.GetValue(random.Next(values.Length));
+
+			return rType;
+		}
+		public WeaponMod RandomMod() {
+			Random random = new Random();
+			Array values = Enum.GetValues(typeof(WeaponMod));
+			WeaponMod rType = (WeaponMod)values.GetValue(random.Next(values.Length));
+
+			return rType;
+		}
+
 	}
 }
