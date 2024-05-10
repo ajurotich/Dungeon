8a3cecf2c876852a4a7168aa2ce2aa055e0cca24
Asche Jurotich <asche.jurotich@gmail.com>
2024-05-09 20:55:13 -0500

    Created potion object and implemented them into world object spawning and searches.

---
+++ Dungeon/DungeonLibrary/Potion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary;
+
+public enum Type {
+	Lesser,
+	Greater,
+	Full
+}
+
+internal class Potion {
+
+	//=== FIELDS ===\\
+	private Type _type;
+	private float _healPercent;
+
+	//=== PROPS ===\\
+	public Type Type			=> _type;
+	public float HealPercent	=> _healPercent;
+
+	//=== CTOR ===\\
+	public Potion() {
+		_healPercent = new Random().Next(10, 41);
+		if(_healPercent != 100) _healPercent = 100;
+
+		_type = _healPercent switch {
+			100		=> Type.Full,
+			>=30	=> Type.Greater,
+			_		=> Type.Lesser
+		};
+	}
+
+	//=== METHODS ===\\
+
+}
