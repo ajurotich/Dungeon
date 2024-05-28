b2f159e48504040c0461d6d228d041397f913686
Asche Jurotich <asche.jurotich@gmail.com>
2024-05-27 21:33:14 -0500

    Entity changes
    Made adjustments with entity class and combat.
    Added score variables that will be tracked on the player and will print at the end of the game. Edited display function for entity to improve look.
    Player class now has a dedicated file.
    Combat file now uses document wide variables for player, enemy, and their cooresponding choices. Functions simplified to reflect change.

--- Dungeon/DungeonLibrary/Entity.cs
+++ Dungeon/DungeonLibrary/Entity.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace DungeonLibrary;
 
@@ -18,6 +17,7 @@ public class Entity {
 	private Race _race;
 	protected Armour _armour;
 	protected Weapon _weapon;
+	protected int _score;
 
 	//=== PROPERTIES ===\\
 	public string Name	 => _name.ToUpper();
@@ -26,6 +26,7 @@ public class Entity {
 	public Race Race	 => _race;
 	public Armour Armour => _armour;
 	public Weapon Weapon => _weapon;
+	public int Score	 => _score;
 
 	//=== CTOR ===\\
 	public Entity(string name, Race race, Armour armour, Weapon weapon) {
@@ -37,28 +38,30 @@ public class Entity {
 
 		_armour = armour;
 		_weapon = weapon;
+
+		_score = (int)((Health + Armour.Defense + Weapon.Damage)
+				 * (new Random().NextSingle() *.25f + .775f));
 	}
 
 	//=== METHODS ===\\
-	public float Damage(float damageAmount) {
-		if(damageAmount <= 0) return Health;
+	public void Damage(float damageAmount) {
+		if(damageAmount <= 0) return;
 
 		_health -= MathF.Round(damageAmount, 1);
+		_score -= (int)(damageAmount * .15f);
 
 		if(Health <= 0) _isAlive = false;
-
-		return Health;
 	}
 
-	public float Heal(float healAmount) {
+	public void Heal(float healAmount) {
 		_health = Math.Clamp((_health += MathF.Round(healAmount, 1)), 0, Race.MaxHealth);
-		return Health;
+		_score += (int)((_health - healAmount) / _health * 10);
 	}
 
 	public void Display() {
 		for(int i= 0; i < 73; i++) Writer.Write("=");
 
-		Writer.WriteLine($"\nDisplaying \'{Name}\' the {Enum.GetName(Race.Type)}:\n");
+		Writer.WriteLine($"\n  Displaying \'{Name}\' the {Enum.GetName(Race.Type)}:\n");
 
 		int p = 16;
 
@@ -74,119 +77,4 @@ public class Entity {
 		Writer.WriteLine("\n");
 	}
 	
-}
-
-public class Player : Entity {
-
-	//=== FIELDS ===\\
-	private int _killCount = 0;
-
-	//=== PROPERTIES ===\\
-	public int KillCount => _killCount;
-
-	//=== CTOR ===\\
-	public Player():
-		base("name",
-		new Race(RaceType.Human),
-		new Armour(ArmourType.Medium),
-		new Weapon(WeaponType.Sword)) {
-	}
-	public Player(string name, Race race)
-		: base(name, race, new Armour(ArmourType.Medium), new Weapon(WeaponType.Sword)) {}
-
-	//=== METHODS ===\\
-	public static Player CreateCharacter() {
-
-		Console.Title = "CHARACTER CREATOR";
-
-		//=== VARIABLES ===\\
-		string? nameChoice;
-		RaceType rType;
-		int charConfirm;
-
-		do {
-			Writer.Clear();
-			Writer.WriteLine("YOUR CHARACTER");
-			//Writer.WriteLine();
-
-			//=== NAME ===\\
-			Writer.Write("\nName: ");
-			int[] cursorPos = [Console.CursorLeft, Console.CursorTop];
-
-			while(true) { 
-				Writer.CursorBottom();
-				Writer.WriteLine("What would you like to name your character?\n");
-				Writer.Write(">> ");
-				nameChoice = Console.ReadLine();
-
-				if(!string.IsNullOrEmpty(nameChoice.Trim())) break;
-			}
-
-			Console.SetCursorPosition(cursorPos[0], cursorPos[1]);
-			Writer.Write(nameChoice.ToUpper());
-			Writer.WriteLine();
-
-			//=== RACE ===\\
-			Writer.Write("Race: ");
-			cursorPos = [Console.CursorLeft, Console.CursorTop];
-			string input;
-
-			while(true) {
-				Writer.CursorBottom();
-				Writer.WriteLine("Choose your race:\n");
-
-				for(int i = 0; i <= (int)RaceType.Orc; i++)
-					Writer.WriteLine($"{i+1}) {(RaceType)i}");
-
-				Writer.WriteLine();
-				Writer.Write(">> ");
-
-				input = Console.ReadLine().Trim().ToUpper();
-				if(Regex.IsMatch(input, "^[1-5]$")) break;
-			}
-
-			rType = input switch {
-				"1" => RaceType.Human,
-				"2" => RaceType.Elf,
-				"3" => RaceType.Dwarf,
-				"4" => RaceType.Goblin,
-				"5" => RaceType.Orc,
-				 _  => Race.RandomType()
-			};			
-
-			Console.SetCursorPosition(cursorPos[0], cursorPos[1]);
-			Writer.Write(Enum.GetName(rType).ToUpper());
-
-
-			//=== CONFIRMATION===\\
-			do {
-				Writer.CursorBottom();
-				Writer.WriteLine($"Does this look correct?\n");
-				Writer.WriteLine($"1) YES\n2) NO");
-
-				Writer.Write("\n>> ");
-				charConfirm = Console.ReadLine().Trim().ToUpper() switch {
-					"1" => 0,
-					"2" => 1,
-					_ => 2
-				};
-			} while(charConfirm == 2);
-
-		} while(charConfirm != 0);
-
-		return new Player(nameChoice, new Race(rType));
-	}
-
-	public void IncrementKillCount() {
-		_killCount++;
-	}
-
-	public void ChangeArmour(Armour armour) {
-		_armour = armour;
-	}
-
-	public void ChangeWeapon(Weapon weapon) {
-		_weapon = weapon;
-	}
-
 }
---
+++ Dungeon/DungeonLibrary/Player.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using System.Text.RegularExpressions;
+
+
+namespace DungeonLibrary;
+
+public class Player : Entity {
+
+	//=== FIELDS ===\\
+	private int _killCount = 0;
+
+	//=== PROPERTIES ===\\
+	public int KillCount => _killCount;
+
+	//=== CTOR ===\\
+	public Player() :
+		base("name",
+		new Race(RaceType.Human),
+		new Armour(ArmourType.Medium),
+		new Weapon(WeaponType.Sword)) {
+	}
+	public Player(string name, Race race)
+		: base(name, race, new Armour(ArmourType.Medium), new Weapon(WeaponType.Sword)) { }
+
+	//=== METHODS ===\\
+	public static Player CreateCharacter() {
+
+		Console.Title = "CHARACTER CREATOR";
+
+		//=== VARIABLES ===\\
+		string nameChoice;
+		RaceType rType;
+		int charConfirm;
+
+		do {
+			Writer.Clear();
+			Writer.WriteLine("YOUR CHARACTER");
+
+			//=== NAME ===\\
+			Writer.Write("\nName: ");
+			int[] cursorPos = [Console.CursorLeft, Console.CursorTop];
+
+			while(true) {
+				Writer.CursorBottom();
+				Writer.WriteLine("What would you like to name your character?\n");
+				Writer.Write(">> ");
+				nameChoice = Console.ReadLine();
+
+				if(!string.IsNullOrEmpty(nameChoice.Trim()))
+					break;
+			}
+
+			Console.SetCursorPosition(cursorPos[0], cursorPos[1]);
+			Writer.Write(nameChoice.ToUpper());
+			Writer.WriteLine();
+
+			//=== RACE ===\\
+			Writer.Write("Race: ");
+			cursorPos = [Console.CursorLeft, Console.CursorTop];
+			string input;
+
+			while(true) {
+				Writer.CursorBottom();
+				Writer.WriteLine("Choose your race:\n");
+
+				for(int i = 0;i <= (int)RaceType.Orc;i++)
+					Writer.WriteLine($"{i+1}) {(RaceType)i}");
+
+				Writer.WriteLine();
+				Writer.Write(">> ");
+
+				input = Console.ReadLine().Trim().ToUpper();
+				if(Regex.IsMatch(input, "^[1-5]$"))
+					break;
+			}
+
+			rType = input switch {
+				"1" => RaceType.Human,
+				"2" => RaceType.Elf,
+				"3" => RaceType.Dwarf,
+				"4" => RaceType.Goblin,
+				"5" => RaceType.Orc,
+				_ => Race.RandomType()
+			};
+
+			Console.SetCursorPosition(cursorPos[0], cursorPos[1]);
+			Writer.Write(Enum.GetName(rType).ToUpper());
+
+
+			//=== CONFIRMATION===\\
+			do {
+				Writer.CursorBottom();
+				Writer.WriteLine($"Does this look correct?\n");
+				Writer.WriteLine($"1) YES\n2) NO");
+
+				Writer.Write("\n>> ");
+				charConfirm = Console.ReadLine().Trim().ToUpper() switch {
+					"1" => 0,
+					"2" => 1,
+					_ => 2
+				};
+			} while(charConfirm == 2);
+
+		} while(charConfirm != 0);
+
+		return new Player(nameChoice, new Race(rType));
+	}
+
+	public void IncrementKillCount() {
+		_killCount++;
+	}
+
+	public void IncreaseScore(int score) => _score += score;
+
+	public void ChangeArmour(Armour armour) {
+		_score += (int)((armour.Defense - _armour.Defense) / _armour.Defense * 10);
+		_score += (int)((armour.Dodge - _armour.Dodge) / _armour.Dodge * 10);
+		_armour = armour;
+	}
+
+	public void ChangeWeapon(Weapon weapon) {
+		_score += (int)((weapon.Damage - _weapon.Damage) / _weapon.Damage * 10);
+		_score += (int)((weapon.Difficulty - _weapon.Difficulty) / _weapon.Difficulty * 10);
+		_weapon = weapon;
+	}
+
+}
+
